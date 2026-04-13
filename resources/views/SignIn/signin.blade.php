60b965874928542d2164f8013bfa1a649831416e
OsvaldoZunniga <osvaldo66239@gmail.com>
2026-04-13 15:20:54 -0600

    Feat: SignIn view added

---
+++ resources/views/SignIn/signin.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href=" {{asset('css/bootstrap.min.css')}} ">
+
+</head>
+
+<body class="bg">
+    <section class="container d-flex justify-content-center align-items-center vh-100">
+        <div class="card col-sm-12 col-md-9 col-lg-6">
+            <div class="card-header">
+                <h5>Registrarse</h5>
+            </div>
+            <div class="card-body">
+                <form class="w-100 d-flex flex-column align-items-center" action="{{url('http://localhost:8001/api/users')}}" method="post">
+                    <label class="col-12 col-md-10 form-label">Nombre Completo
+                        <input class="form-control" type="text" name="full_name">
+                    </label>
+                    <input type="hidden" value="3" name="role_id">
+
+                    <label class="col-12 col-md-10 form-label">Email
+                        <input class="form-control" type="email" name="email">
+                    </label>
+
+                    <label class="col-12 col-md-10 form-label">Telefono
+                        <input class="form-control" type="text" name="phone">
+                    </label>
+
+                    <div class="d-flex gap-3 col-12 col-md-10">
+                        <label class="form-label w-50">Contraseña
+                            <input class="form-control" type="password" name="password">
+                        </label>
+                        <label class="form-label w-50">Confirmar contraseña
+                            <input class="form-control" type="password">
+                        </label>
+                    </div>
+                    <div class="col-10 d-flex justify-content-center gap-3">
+                        <button type="submit" class="btn btn-success col-5 ">Registrarse</button>
+                        <a class="btn btn-danger col-5" href="{{ url('/')}}">Cancelar</a>
+                    </div>
+                </form>
+            </div>
+        </div>
+
+    </section>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('login');
-});
+})->name("index");
+
+Route::get('/SignIn', function () {
+    return view('SignIn.signin');
+})->name("SignIn");
