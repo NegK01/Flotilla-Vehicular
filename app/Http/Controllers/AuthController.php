9248bfc93cebfbec08ffedc27c71054e35bc3796
Enoc <esquivelenoc20@gmail.com>
2026-04-13 19:06:43 -0600

    chore: new file called auth

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
+
+class AuthController extends Controller
+{
+    //
+}
