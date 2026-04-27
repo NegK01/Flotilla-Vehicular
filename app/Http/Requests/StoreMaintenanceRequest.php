6839a3a7c2c9026eaa273ce1f2005e22bd3d8bfc
Enoc <esquivelenoc20@gmail.com>
2026-04-26 23:49:19 -0600

    Feat: requests of maintenances created

---
+++ app/Http/Requests/StoreMaintenanceRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreMaintenanceRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'type' => 'required|in:preventive,corrective',
+            'start_at' => 'required|date',
+            'closed_at' => 'sometimes|date|after:start_at',
+            'description' => 'required|string',
+            'cost' => 'sometimes|nullable|numeric|min:0|max:99999999.99',
+            'status' => 'sometimes|in:open,closed',
+        ];
+    }
+}
